8c6df45d39fd770b24a3cd92e1bff6f210e6c5e3
abey onalaja <abey.onalaja@gmail.com>
2024-06-18 21:18:46 +0100

    Sign in screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,12 +3,21 @@ import React, { useState } from "react";
 
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignIn = () => {
   const [form, setform] = useState({
     email: "",
     password: "",
   });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {
+    console.log(form);
+  };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -35,6 +44,23 @@ const SignIn = () => {
             handleChangeText={(e) => setform({ ...form, password: e })}
             otherStyles="mt-7"
           />
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-4"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 gap-2 flex-row">
+            <Text className="text-gray-100 text-lg font-pregular">
+              Don't have an account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign up
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
