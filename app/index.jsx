e47ce3d8cafee97b0bb23acabcfbdf8f59a4439b
abey onalaja <abey.onalaja@gmail.com>
2024-06-18 18:06:12 +0100

    update tabs

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,11 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <ScrollView contentContainerStyle={{ height: "100%" }}></ScrollView>
+    </SafeAreaView>
   );
 }
